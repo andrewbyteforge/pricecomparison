aff04d812301afb753591a070792776e96a5a6fc
andrewbyteforge <andrewbyteforge@gmail.com>
2024-01-28 17:24:37 +0000

    Just tidying up

--- Tea_Lovers/Tea_Lovers/settings.py
+++ Tea_Lovers/Tea_Lovers/settings.py
@@ -60,10 +60,12 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'Tea_Lovers.urls'
 
+import os
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],  # Add this line
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
